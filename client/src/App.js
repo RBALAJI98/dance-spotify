8e0bd14ce5234228afbbe25ded8137f3c47a3f49
Rohan <rohan@obexsolutions.com>
2018-07-17 17:24:10 -0500

    added api endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
   constructor(){
     super();
     const params = this.getHashParams();
-    console.log(params);
+    const token = params.access_token;
+    if(token){
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: {name:'Not Checked', albumArt: ''}
+    }
   }
   getHashParams() {
     var hashParams = {};
@@ -23,9 +32,37 @@ class App extends Component {
     return (
       <div className="App">
         <a href='http://localhost:8888/login' > Login to Spotify </a>
+        <div>
+          Now Playing: {this.state.nowPlaying.name}
+        </div>
+        <div>
+        <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+      
+      
+      </div>
+
+      { this.state.loggedIn &&
+        <button onClick={() => this.getNowPlaying()}>
+          Check Now Playing
+        </button>
+      }
+      
       </div>
     );
   }
-}
+
+  getNowPlaying(){
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        this.setState({
+          nowPlaying: { 
+              name: response.item.name, 
+              albumArt: response.item.album.images[0].url
+            }
+        });
+      })
+  }
+
+
 
 export default App;
