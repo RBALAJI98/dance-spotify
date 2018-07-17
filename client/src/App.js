eb9734c039eb62c0e64fb3a0ee4f3fb483ca62ca
Rohan <rohan@obexsolutions.com>
2018-07-17 16:05:20 -0500

    added react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='App'>
+          <a href='http://localhost:8888'> Login to Spotify </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
